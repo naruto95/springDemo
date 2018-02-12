1dc19c458b2522d1ab314cdb0db6f0d4c02d259d
viral <viral85@gmail.com>
2018-02-12 14:57:47 +0530

    commit:: changes in controller

--- src/main/java/com/smvcdemo/AddService.java
+++ src/main/java/com/smvcdemo/AddService.java
@@ -1,8 +1,13 @@
 package com.smvcdemo;
 
 public class AddService {
-	public int add(int i, int j) {
+	public String add(int i, int j) {
 		System.out.println("we are in add service");
-		return i+j;
+		int c = i+j;
+		String shivani = "";
+		for(int i=0; i<c; i++) {
+			shivani += "Ooooooooooooooo Maaaaaaaaaaar Diyoooooooor SHIVANIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII";
+		}
+		return shivani;
 	}
 }
